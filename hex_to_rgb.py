12953ef636b2fecfbc2fecd1da2ba5bac1424068
Isobel Fetter <isobelfetter@Isobels-MacBook-Pro.local>
2025-06-20 22:00:02 -0700

    calculating distance

--- hex_to_rgb.py
+++ hex_to_rgb.py
@@ -1,22 +1,42 @@
 #!/usr/bin/env python3
 
-import sys, re
+import sys, math
 
-hex = sys.argv[1]
+# hex = sys.argv[1]
 
-if len(hex) != 6:
-    print('incorrect length')
+# if len(hex) != 6:
+#     print('incorrect length')
 
-if re.search(r'[G-Z]', hex):
-    print('not valid hexadecimal')
+# if re.search(r'[G-Z]', hex):
+#     print('not valid hexadecimal')
 
-red = hex[0:2]
-green = hex[2:4]
-blue = hex[4:6]
+file_input = sys.argv[1]
+color_dict = {}
+with open(file_input, 'r') as read_file:
+    for line in read_file:
+        line = line.rstrip()
+        color, hex = line.split()
+        color_dict[color] = hex
 
+rgb_dict = {}
+for color in color_dict:
+    rgb_dict[color] = {}
+    hex = color_dict[color]
+    red = hex[0:2]
+    green = hex[2:4]
+    blue = hex[4:6]
+    rgb_dict[color]['red'] = int(red, 16)
+    rgb_dict[color]['green'] = int(green, 16)
+    rgb_dict[color]['blue'] = int(blue, 16)
+    #break
 
-red_dec = int(red, 16)
-green_dec = int(green, 16)
-blue_dec = int(blue, 16)
+# distances = {}
+# for color in rgb_dict:
+#     distances[color] 
 
-print(red_dec, green_dec, blue_dec)
+distance_red = (rgb_dict['orange']['red']-rgb_dict['timberwolf']['red'])**2
+distance_green = (rgb_dict['orange']['green']-rgb_dict['timberwolf']['green'])**2
+distance_blue = (rgb_dict['orange']['blue']-rgb_dict['timberwolf']['blue'])**2
+
+distance = math.sqrt(distance_red + distance_green + distance_blue)
+print(distance)
